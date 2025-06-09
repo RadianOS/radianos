a821a2c1269933d8bf39286138be24d2910fe8d4
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-06-09 16:45:36 +0200

    Fix

--- system/core/src/log.rs
+++ system/core/src/log.rs
@@ -3,16 +3,17 @@ use asm_libs::inportb;
 use asm_libs::outportb;
 
 
-fn serial_ready() -> u8 {
-  return inportb(0x3F8 + 5) & 0x20; // check 0x3F8 is able to write.
-}
+mod log {
+  fn serial_ready() -> bool {
+    return inportb(0x3F8 + 5) & 0x20 != 0; // check 0x3F8 is able to write.
+  }
 
-fn serial_write(c: char) -> bool {
-  while !serial_ready() {}
-  outportb(0x3F8, c as u8);
-}
+  fn serial_write(c: char) {
+    while !serial_ready() {}
+    outportb(0x3F8, c as u8);
+  }
 
-fn log_init() {
+  pub fn init() {
     outportb(0x3F8 + 1, 0x00); // disable interrupts
     
     outportb(0x3F8 + 3, 0x80);
@@ -22,12 +23,13 @@ fn log_init() {
     outportb(0x3F8 + 2, 0xC7);
     
     outportb(0x3F8 + 4, 0x0B);
-}
+  }
 
-fn log_print(msg: Option<&str>) {
-  if msg.is_none() { return; }
-
-  for c in msg.unwrap().chars() { // write all characters one by one.. TODO: Add formatting.
-        serial_write(c);
+  pub fn print(msg: Option<&str>) {
+    if msg.is_none() { return; }
+  
+    for c in msg.unwrap().chars() { // write all characters one by one.. TODO: Add formatting.
+          serial_write(c);
+    }
   }
 }
