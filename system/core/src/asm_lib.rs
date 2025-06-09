86de73392f748f844a642015eb5ef33bc5951e59
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-06-09 16:52:05 +0200

    Fix

--- system/core/src/asm_lib.rs
+++ system/core/src/asm_lib.rs
@@ -1,15 +1,17 @@
-pub unsafe fn outportb(const port: u16, const data: u8) {
-        core::arch::naked_asm!(
-            asm("outb %1, %0" : : "dN" (port), "a" (data));
-        )
-    }
-
-pub unsafe fn inportb(const port: u16) {
-        core::arch::naked_asm!(
-            u8 r;
-            asm("inb %1, %0" : "=a" (r) : "dN" (port));
-            return r;
-        )
-
+mod asm {
+        pub unsafe fn outportb(port: u16, data: u8) {
+                core::arch::naked_asm!(
+                    asm("outb %1, %0" : : "dN" (port), "a" (data));
+                )
+            }
         
-    }
+        pub unsafe fn inportb(port: u16) {
+                core::arch::naked_asm!(
+                    u8 r;
+                    asm("inb %1, %0" : "=a" (r) : "dN" (port));
+                    return r;
+                )
+        
+                
+            }
+}
